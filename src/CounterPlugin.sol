21747818e1d0e89eaf87530f519c99ab870b6ebc
Dan Nolan <dan.nolan@alchemy.com>
2024-02-28 22:56:26 -0500

    adds helpful comments to our counter plugin

--- src/CounterPlugin.sol
+++ src/CounterPlugin.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.19;
 
-import {UserOperation} from "@eth-infinitism/account-abstraction/interfaces/UserOperation.sol";
 import {BasePlugin} from "@alchemy/modular-account/src/plugins/BasePlugin.sol";
 import {IPluginExecutor} from "@alchemy/modular-account/src/interfaces/IPluginExecutor.sol";
-import {SIG_VALIDATION_PASSED} from "@alchemy/modular-account/src/libraries/Constants.sol";
 import {
     ManifestFunction,
     ManifestAssociatedFunctionType,
@@ -18,16 +16,31 @@ import {
 /// @author Alchemy
 /// @notice This plugin lets increment a count!
 contract CounterPlugin is BasePlugin {
+    // metadata used by the pluginMetadata() method down below
     string public constant NAME = "Counter Plugin";
     string public constant VERSION = "1.0.0";
     string public constant AUTHOR = "Alchemy";
 
+    // this is a constant used in the manifest, to reference our only dependency: the multi owner plugin
+    // since it is the first, and only, plugin the index 0 will reference the multi owner plugin
+    // we can use this to tell the modular account that we should use the multi owner plugin to validate our user op
+    // in other words, we'll say "make sure the person calling increment is an owner of the account using our multiowner plugin"
+    uint256 internal constant _MANIFEST_DEPENDENCY_INDEX_OWNER_USER_OP_VALIDATION = 0;
+
+    // ERC 7562 defines the associated storage rules for ERC 4337 accounts
+    // and plugins need to follow this definition to have succesful user ops executed by a bundler
+    // a mapping from the account address is considered associated storage
+    // see the definition here: https://eips.ethereum.org/EIPS/eip-7562#validation-rules
     mapping(address => uint256) public count;
 
     // ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
     // ┃    Execution functions    ┃
     // ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 
+    // this is the one thing we are attempting to do with our plugin!
+    // we define increment to modify our associated storage, count
+    // then in the manifest we define it as an execution function,
+    // and we specify the validation function for the user op targeting this function
     function increment() external {
         count[msg.sender]++;
     }
@@ -37,29 +50,39 @@ contract CounterPlugin is BasePlugin {
     // ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 
     /// @inheritdoc BasePlugin
-    // solhint-disable-next-line no-empty-blocks
     function onInstall(bytes calldata) external pure override {}
 
     /// @inheritdoc BasePlugin
-    // solhint-disable-next-line no-empty-blocks
     function onUninstall(bytes calldata) external pure override {}
 
     /// @inheritdoc BasePlugin
     function pluginManifest() external pure override returns (PluginManifest memory) {
         PluginManifest memory manifest;
 
+        // since we are using the modular account, we will specify one depedency
+        // which will be the multiowner plugin
+        // you can find this depedency specified in the installPlugin call in the tests
         manifest.dependencyInterfaceIds = new bytes4[](1);
         manifest.dependencyInterfaceIds[0] = type(IPlugin).interfaceId;
 
+        // we only have one execution function that can be called, which is the increment function
+        // here we define that increment function on the manifest as something that can be called during execution
         manifest.executionFunctions = new bytes4[](1);
         manifest.executionFunctions[0] = this.increment.selector;
 
+        // you can think of ManifestFunction as a reference to a function somewhere,
+        // we want to say "use this function" for some purpose - in this case,
+        // we'll be using the user op validation function from the multi owner dependency
+        // and this is specified by the depdendency index
         ManifestFunction memory ownerUserOpValidationFunction = ManifestFunction({
             functionType: ManifestAssociatedFunctionType.DEPENDENCY,
             functionId: 0, // unused since it's a dependency
-            dependencyIndex: 0 // <-- the index of the multi owner function
+            dependencyIndex: _MANIFEST_DEPENDENCY_INDEX_OWNER_USER_OP_VALIDATION
         });
 
+        // here we will link together the increment function with the multi owner user op validation
+        // this basically says "use this user op validation function and make sure everythings okay before calling increment"
+        // this will ensure that only an owner of the account can call increment
         manifest.userOpValidationFunctions = new ManifestAssociatedFunction[](1);
         manifest.userOpValidationFunctions[0] = ManifestAssociatedFunction({
             executionSelector: this.increment.selector,
