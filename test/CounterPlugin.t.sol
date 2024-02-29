9742a59e132f1e690f49bfb2b7199b866e6cf3f0
Dan Nolan <dan.nolan@alchemy.com>
2024-02-28 23:09:28 -0500

    adds comments to our tests as well

--- test/CounterPlugin.t.sol
+++ test/CounterPlugin.t.sol
@@ -30,9 +30,13 @@ contract CounterTest is Test {
     uint256 constant VERIFICATION_GAS_LIMIT = 1000000;
 
     function setUp() public {
+        // we'll be using the entry point so we can send a user operation through
+        // in this case our plugin only accepts calls to increment via user operations so this is essential
         entryPoint = IEntryPoint(address(new EntryPoint()));
 
-        // setup plugins and factory
+        // our modular smart contract account will be installed with the multi owner plugin
+        // so we have a way to determine who is authorized to do things on this account
+        // we'll use this plugin's validation for our increment function
         MultiOwnerPlugin multiOwnerPlugin = new MultiOwnerPlugin();
         MultiOwnerModularAccountFactory factory = new MultiOwnerModularAccountFactory(
             address(this),
@@ -42,22 +46,30 @@ contract CounterTest is Test {
             entryPoint
         );
 
+        // the beneficiary of the fees at the entry point
         beneficiary = payable(makeAddr("beneficiary"));
 
+        // create a single owner for this account and provide the address to our modular account
+        // we'll also add ether to our account to pay for gas fees
         (owner1, owner1Key) = makeAddrAndKey("owner1");
         owners = new address[](1);
         owners[0] = owner1;
         account1 = UpgradeableModularAccount(payable(factory.createAccount(0, owners)));
         vm.deal(address(account1), 100 ether);
 
+        // create our counter plugin and grab the manifest hash so we can install it
+        // note: plugins are singleton contracts, so we only need to deploy them once
         counterPlugin = new CounterPlugin();
         bytes32 manifestHash = keccak256(abi.encode(counterPlugin.pluginManifest()));
 
+        // we will have a single function dependency for our counter contract: the multi owner user op validation
+        // we'll use this to ensure that only an owner can sign a user operation that can successfully increment
         FunctionReference[] memory dependencies = new FunctionReference[](1);
         dependencies[0] = FunctionReferenceLib.pack(
             address(multiOwnerPlugin), uint8(IMultiOwnerPlugin.FunctionId.USER_OP_VALIDATION_OWNER)
         );
 
+        // install this plugin on the account as the owner
         vm.prank(owner1);
         account1.installPlugin({
             plugin: address(counterPlugin),
@@ -68,6 +80,7 @@ contract CounterTest is Test {
     }
 
     function test_Increment() public {
+        // create a user operation which has the calldata to specify we'd like to increment
         UserOperation memory userOp = UserOperation({
             sender: address(account1),
             nonce: 0,
@@ -82,15 +95,17 @@ contract CounterTest is Test {
             signature: ""
         });
 
+        // sign this user operation with the owner, otherwise it will revert due to the multiowner validation
         bytes32 userOpHash = entryPoint.getUserOpHash(userOp);
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(owner1Key, userOpHash.toEthSignedMessageHash());
         userOp.signature = abi.encodePacked(r, s, v);
 
+        // send our single user operation to increment our count
         UserOperation[] memory userOps = new UserOperation[](1);
         userOps[0] = userOp;
-
         entryPoint.handleOps(userOps, beneficiary);
 
+        // check that we successfully incremented!
         assertEq(counterPlugin.count(address(account1)), 1);
     }
 }
